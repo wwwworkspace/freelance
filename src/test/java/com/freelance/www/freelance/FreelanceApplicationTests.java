56280ac6b0675353e563f5826a9781d76644c37d
www <www@lychee.wang>
2020-05-02 16:45:30 +0800

    11

--- src/test/java/com/freelance/www/freelance/FreelanceApplicationTests.java
+++ src/test/java/com/freelance/www/freelance/FreelanceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class FreelanceApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(String flag) {
     }
 
 }
