41675f435f85375dd4debeb16e2008d63571b8f6
www <www@lychee.wang>
2020-05-02 16:26:13 +0800

    初次提交

---
+++ src/test/java/com/freelance/www/freelance/FreelanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelance.www.freelance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
